a48a40d6fcc413db3533700ca992feec35eaeec8
Abdul Dayur <Abdul.dayur@nike.com>
2021-07-10 13:02:39 -0700

    created favorite list vc

---
+++ GithubFollowers/FavoritesListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritesListViewController.swift
+//  GithubFollowers
+//
+//  Created by Abdul Dayur on 7/10/21.
+//
+
+import UIKit
+
+class FavoritesListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBlue
+    }
+}
