6705b102ab6b08614e6a9b76247db4f8ed8d1fd0
Abdul Dayur <Abdul.dayur@nike.com>
2021-07-10 13:02:16 -0700

    create search vc

---
+++ GithubFollowers/SearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchViewController.swift
+//  GithubFollowers
+//
+//  Created by Abdul Dayur on 7/10/21.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPink
+    }
+
+}
