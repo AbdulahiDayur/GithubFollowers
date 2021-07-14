2b8c2bb228d7fe6860fe0b6b83b5cc2d35f336cf
Abdul Dayur <Abdul.dayur@nike.com>
2021-07-13 18:12:45 -0700

    added follower object for json data retrieval

---
+++ GithubFollowers/Model/Follower.swift
@@ -0,0 +1,15 @@
+//
+//  Follower.swift
+//  GithubFollowers
+//
+//  Created by Abdul Dayur on 7/13/21.
+//
+
+import Foundation
+
+
+struct Follower: Codable {
+    
+    var login: String
+    var avatarUrl: String
+}
