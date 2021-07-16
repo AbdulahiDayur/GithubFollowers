8f3911dab273e5598994ae26b7dce536e6581187
Abdul Dayur <Abdul.dayur@nike.com>
2021-07-16 11:27:22 -0700

    conforming to result type for completion handler

--- GithubFollowers/Screens/FollowerListViewController.swift
+++ GithubFollowers/Screens/FollowerListViewController.swift
@@ -18,17 +18,17 @@ class FollowerListViewController: UIViewController {
         navigationController?.navigationBar.prefersLargeTitles = true
         
         
-        NetworkManager.shared.getFollowers(for: username, page: 1) { (followers, errorMessage) in
-
-            guard let followers = followers else {
-                self.presentGFAlertOnMainThread(title: "Bad Stuff Happend", message: errorMessage!, buttonTitle: "OK")
-                return
+        NetworkManager.shared.getFollowers(for: username, page: 1) { (result) in
+            
+            switch result {
+            
+            case .success(let followers):
+                print(followers)
+                
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Bad Stuff Happend", message: error.rawValue, buttonTitle: "OK")
             }
-
-            print("Followers.count = \(followers.count)")
-            print(followers)
         }
-
     }
     
     override func viewWillAppear(_ animated: Bool) {
