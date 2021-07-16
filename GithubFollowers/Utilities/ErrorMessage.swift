9f10d1b23b75902be0a8c3e78f21283de8318c82
Abdul Dayur <Abdul.dayur@nike.com>
2021-07-16 11:27:52 -0700

    using enum to handle errors for our network call

---
+++ GithubFollowers/Utilities/ErrorMessage.swift
@@ -0,0 +1,16 @@
+//
+//  ErrorMessage.swift
+//  GithubFollowers
+//
+//  Created by Abdul Dayur on 7/15/21.
+//
+
+import Foundation
+
+enum GFError: String, Error {
+    
+    case invalidUsername = "This username created an invalid request. Please try again"
+    case unableToComplete = "Unable to complete your request. Please try again. Please check your internet connection."
+    case invalidResponse = "Invalid response from the server. Please try again."
+    case invalidData = "The data received  from the server was invalid. Please try again"
+}
