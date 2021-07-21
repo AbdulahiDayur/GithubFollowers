8e765a7084db0e61a82274559f8907cf837a2a6d
Abdul Dayur <Abdul.dayur@nike.com>
2021-07-21 13:44:11 -0700

    using [weak self] inside closure to avoid memory leak

--- GithubFollowers/Screens/FollowerListViewController.swift
+++ GithubFollowers/Screens/FollowerListViewController.swift
@@ -40,30 +40,18 @@ class FollowerListViewController: UIViewController {
     }
     
     func configureCollectionView() {
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayOut())
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UIHelper.createThreeColumnFlowLayOut(in: view))
         view.addSubview(collectionView)
         collectionView.backgroundColor = .systemBackground
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.resuseID)
     }
-    
-    func createThreeColumnFlowLayOut() -> UICollectionViewFlowLayout {
-        
-        let width = view.bounds.width
-        let padding: CGFloat = 12
-        let minimumItemSpacing: CGFloat = 10
-        let availablewidth = width - (padding * 2) - (minimumItemSpacing * 2)
-        let itemWidth = availablewidth / 3
-        
-        let flowLayout = UICollectionViewFlowLayout()
-        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
-        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
-        
-        return flowLayout
-    }
+
     
     func getFollowers() {
         
-        NetworkManager.shared.getFollowers(for: username, page: 1) { (result) in
+        NetworkManager.shared.getFollowers(for: username, page: 1) { [weak self] (result) in
+            
+            guard let self = self else {return}
             
             switch result {
             
@@ -83,6 +71,7 @@ class FollowerListViewController: UIViewController {
             
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FollowerCell.resuseID, for: indexPath) as! FollowerCell
             cell.set(follower: follower)
+//            cell.usernameLabel.text = follower.login
             
             return cell
             
