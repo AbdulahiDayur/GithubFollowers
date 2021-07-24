21f8cc89a33c0943d29d516b79bf1fbc7ea75cc6
Abdul Dayur <Abdul.dayur@nike.com>
2021-07-23 17:23:07 -0700

    calling activity indicator func right before network call

--- GithubFollowers/Screens/FollowerListViewController.swift
+++ GithubFollowers/Screens/FollowerListViewController.swift
@@ -52,14 +52,17 @@ class FollowerListViewController: UIViewController {
     
     func getFollowers(username: String, page: Int) {
         
+        showLoadingView()
+        
         NetworkManager.shared.getFollowers(for: username, page: page) { [weak self] (result) in
             
             guard let self = self else {return}
+            self.dismissLoadingView()
             
             switch result {
             
             case .success(let followers):
-                if followers.count < 100 {self.hasMoreFollowers = false}
+                if followers.count < 20 {self.hasMoreFollowers = false}
                 self.followers.append(contentsOf: followers)
                 self.updateData()
                 
