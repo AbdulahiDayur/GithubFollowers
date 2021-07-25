a11a66dfb7ebccf91aca4fec42ffefcce53677a9
Abdul Dayur <Abdul.dayur@nike.com>
2021-07-24 22:09:30 -0700

    checking if user has no followers and showing them a message if true

--- GithubFollowers/Screens/FollowerListViewController.swift
+++ GithubFollowers/Screens/FollowerListViewController.swift
@@ -64,6 +64,15 @@ class FollowerListViewController: UIViewController {
             case .success(let followers):
                 if followers.count < 20 {self.hasMoreFollowers = false}
                 self.followers.append(contentsOf: followers)
+                
+                if followers.isEmpty {
+                    let message = "This user doesn't have any followers. Go follow them."
+                    DispatchQueue.main.async {
+                        self.showEmptyStateView(with: message, in: self.view)
+                    }
+                    return
+                }
+                
                 self.updateData()
                 
             case .failure(let error):
