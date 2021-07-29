cb939e44151adcf04c7982a2fabfc56b78663f68
Abdul Dayur <Abdul.dayur@nike.com>
2021-07-29 12:01:50 -0700

    making a network call to fetch necessary user data

--- GithubFollowers/Screens/UserInfoViewController.swift
+++ GithubFollowers/Screens/UserInfoViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class UserInfoViewController: UIViewController {
+    
+    var username: String!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,6 +18,18 @@ class UserInfoViewController: UIViewController {
         
         let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
         navigationItem.rightBarButtonItem = doneButton
+        
+        NetworkManager.shared.getUserInfo(for: username) { [weak self] (result) in
+            guard let self = self else {return}
+            
+            switch result {
+            case .success(let user):
+                print(user)
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonTitle: "Ok")
+            
+            }
+        }
         
     }
     
