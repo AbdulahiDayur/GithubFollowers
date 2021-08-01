6c3f2eefad43f64a5c72efcae3b0a189a58a0295
Abdul Dayur <Abdul.dayur@nike.com>
2021-07-31 18:59:17 -0700

    new file for reusable constants

---
+++ GithubFollowers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GithubFollowers
+//
+//  Created by Abdul Dayur on 7/31/21.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
