775f3b8ae37ea3b89edb1888c2b18d8814ffe907
Abdul Dayur <Abdul.dayur@nike.com>
2021-08-04 12:02:06 -0700

    new subclass for repoItem

---
+++ GithubFollowers/Custom Views/ViewControllers/GFRepoItemViewController.swift
@@ -0,0 +1,13 @@
+//
+//  GFRepoItemViewController.swift
+//  GithubFollowers
+//
+//  Created by Abdul Dayur on 8/4/21.
+//
+
+import UIKit
+
+class GFRepoItemViewController: GFItemInfoViewController {
+    
+    
+}
