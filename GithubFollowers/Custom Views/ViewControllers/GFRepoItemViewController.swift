77347aba056ce401d38db37044238bd668a5899c
Abdul Dayur <Abdul.dayur@nike.com>
2021-08-04 15:35:04 -0700

    configuring user repo information

--- GithubFollowers/Custom Views/ViewControllers/GFRepoItemViewController.swift
+++ GithubFollowers/Custom Views/ViewControllers/GFRepoItemViewController.swift
@@ -9,5 +9,15 @@ import UIKit
 
 class GFRepoItemViewController: GFItemInfoViewController {
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureItems()
+        
+    }
     
+    private func configureItems() {
+        itemInfoViewOne.set(itemInfoType: .repos, withCount: user.publicRepos)
+        itemInfoViewTwo.set(itemInfoType: .gists, withCount: user.publicGists)
+        actionButton.set(backgroundColor: .systemPurple, title: "GitHub Profile")
+    }
 }
