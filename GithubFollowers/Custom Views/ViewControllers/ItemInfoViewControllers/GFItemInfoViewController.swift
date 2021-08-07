15381de75ab4eacf881a1658b20192e40bb28da3
Abdul Dayur <Abdul.dayur@nike.com>
2021-08-07 16:59:46 -0700

    setting userInfo delegate in itemInfo superclass

--- GithubFollowers/Custom Views/ViewControllers/ItemInfoViewControllers/GFItemInfoViewController.swift
+++ GithubFollowers/Custom Views/ViewControllers/ItemInfoViewControllers/GFItemInfoViewController.swift
@@ -17,6 +17,7 @@ class GFItemInfoViewController: UIViewController {
     let actionButton = GFButton()
     
     var user: User!
+    var delegate: UserInfoVCDelegate!
     
     init(user: User) {
         super.init(nibName: nil, bundle: nil)
@@ -30,6 +31,7 @@ class GFItemInfoViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configureBackgroundView()
+        configureActionButton()
         layoutUI()
         configureStackView()
     }
@@ -46,6 +48,12 @@ class GFItemInfoViewController: UIViewController {
         stackView.addArrangedSubview(itemInfoViewOne)
         stackView.addArrangedSubview(itemInfoViewTwo)
     }
+    
+    private func configureActionButton() {
+        actionButton.addTarget(self, action: #selector(actionButtonTapped), for: .touchUpInside)
+    }
+    
+    @objc func actionButtonTapped(){}
     
     private func layoutUI() {
         view.addSubview(stackView)
