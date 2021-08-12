59b3407cd56c1fe638f2dedb632d7eba56cd4f9e
Abdul Dayur <Abdul.dayur@nike.com>
2021-08-12 13:44:43 -0700

    created function that updates favorites Lists

--- GithubFollowers/Managers/PersistenceManager.swift
+++ GithubFollowers/Managers/PersistenceManager.swift
@@ -7,12 +7,40 @@
 
 import Foundation
 
+enum PersistenceActionType {
+    case add, remove
+}
+
 enum PersistenceManager {
     static private let defaults = UserDefaults.standard
     
     enum Keys {
         static let favorites = "favorites"
     }
+    
+    
+    static func updateWith(favorite: Follower, actionType: PersistenceActionType, completed: @escaping (GFError?) -> Void) {
+        retrieveFavorites { (result) in
+            switch result {
+            case .success(let favorites):
+                var retrievedFavorites = favorites
+                
+                switch actionType {
+                case .add:
+                    break
+                    
+                    
+                case .remove:
+                    break
+                }
+                
+                
+            case .failure(let error):
+                completed(error)
+            }
+        }
+    }
+    
     
     static func retrieveFavorites(completed: @escaping (Result<[Follower], GFError>) -> Void) {
         guard let favoritesData = defaults.object(forKey: Keys.favorites) as? Data else {
@@ -30,8 +58,16 @@ enum PersistenceManager {
         
     }
     
-    static func saveFavorites() {
+    static func saveFavorites(favorites: [Follower]) -> GFError? {
         
+        do {
+            let encoder = JSONEncoder()
+            let endcodedFavorites = try encoder.encode(favorites)
+            defaults.set(endcodedFavorites, forKey: Keys.favorites)
+            return nil
+        } catch {
+            return .unableToFavorite
+        }
     }
 }
 
