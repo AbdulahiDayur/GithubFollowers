aeaab59ef5c1f10cbe76fe1b0ab04183f4b659c7
Abdul Dayur <Abdul.dayur@nike.com>
2021-08-20 10:34:27 -0700

    refactoring tabBarVC

---
+++ GithubFollowers/Custom Views/TabBarControllers/GFTabBarController.swift
@@ -0,0 +1,37 @@
+//
+//  GFTabBarController.swift
+//  GithubFollowers
+//
+//  Created by Abdul Dayur on 8/19/21.
+//
+
+import UIKit
+
+class GFTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        UITabBar.appearance().tintColor = .systemGreen
+        viewControllers = [createSearchNC(), createFavoritesNC()]
+        
+    }
+    
+    func createSearchNC() -> UINavigationController {
+        
+        let searchVC = SearchViewController()
+        searchVC.title = "Search"
+        searchVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
+        
+        return UINavigationController(rootViewController: searchVC)
+    }
+    
+    func createFavoritesNC() -> UINavigationController {
+        
+        let favoriteListVC = FavoritesListViewController()
+        favoriteListVC.title = "Favorites List"
+        favoriteListVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+        
+        return UINavigationController(rootViewController: favoriteListVC)
+    }
+
+}
